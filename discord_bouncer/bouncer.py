a06f37658ccf1b8715e53f1122c3cb3a69edf90c
jmurrah <jacobhmurrah@gmail.com>
2024-04-27 15:26:51 -0500

    able to connect to discord bot!

---
+++ discord_bouncer/bouncer.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+
+client = discord.Client(intents=discord.Intents.default())
+
+
+async def add_role(context: commands.Context, member: discord.Member, role: discord.Role):
+    await member.add_roles(role)
+    await context.send(f"Successfully gave the role {role.name} to {member.name}")
+
+
+async def remove_role(
+    context: commands.Context, member: discord.Member, role: discord.Role
+):
+    await member.remove_roles(role)
+    await context.send(f"Successfully removed the role {role.name} from {member.name}")
+
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+
+
+def main():
+    load_dotenv()
+    client.run(os.getenv("DISCORD_KEY"))
+
+    user = 
+
+    client.add_role = add_role()
+    breakpoint()
