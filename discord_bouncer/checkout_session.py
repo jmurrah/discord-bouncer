65ce1553127f526f6a2cf6da46af38daff68d93a
jmurrah <jacobhmurrah@gmail.com>
2024-05-03 21:57:38 -0500

    need to figure out how to create a subscription link

--- discord_bouncer/checkout_session.py
+++ discord_bouncer/checkout_session.py
@@ -1,36 +1,42 @@
 import stripe
 from dotenv import load_dotenv
 import os
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+PAID_ROLE = "secret_chat"
+ROLE_PRICE = 500  # in cents = $5.00
 
 
-def create_checkout_session() -> stripe.checkout.Session:
-    session = stripe.checkout.Session.create(
-        payment_method_types=["card"],
-        line_items=[
-            {
-                "price_data": {
-                    "currency": "usd",
-                    "product_data": {
-                        "name": "T-shirt",
-                    },
-                    "unit_amount": 2000,
-                },
-                "quantity": 1,
-            },
-        ],
-        mode="payment",
-        success_url="http://127.0.0.1:5000/checkout/success",
-        cancel_url="http://127.0.0.1:5000/checkout/cancel",
+def get_role_access_dates() -> tuple[date, date]:
+    today = date.today()
+    return today, today + relativedelta(months=1)
+
+
+def create_payment_link() -> stripe.PaymentLink:
+    start_date, end_date = get_role_access_dates()
+
+    product = stripe.Product.create(
+        name=f"{PAID_ROLE} Discord Role",
+        description=(
+            f"Access to the {PAID_ROLE} Discord Role for 1 month.\n"
+            f"You will have access from {start_date.strftime('%B %d, %Y')} to {end_date.strftime('%B %d, %Y')}"
+        )
     )
-    return session.id
+    breakpoint()
+    price = stripe.Price.create(
+        unit_amount=ROLE_PRICE, currency="usd", product_data=product.id
+    )
+
+    return stripe.PaymentLink.create(line_items=[{"price": price.id, "quantity": 1}])
 
 
 def handle_checkout_session():
     load_dotenv()
     stripe.api_key = os.getenv("STRIPE_KEY")
 
-    session_id = create_checkout_session()
-    breakpoint()
+    payment_link = create_payment_link()
+    print(payment_link.url)
     # session = stripe.checkout.Session.retrieve(session_id)
     # if session.payment_status == "paid":
     #     return add_role(session.customer)
--- discord_bouncer/discord_bouncer.py
+++ discord_bouncer/discord_bouncer.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
-from .checkout_session import handle_checkout_session
+from .checkout_session import handle_checkout_session, PAID_ROLE
 from .database import get_paid_users
 
 intents = discord.Intents.default()
@@ -11,10 +11,10 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-PAID_ROLE = "secret_chat"
 
-
-async def add_role(channel: discord.Message.channel, member: discord.Member, role: discord.Role):
+async def add_role(
+    channel: discord.Message.channel, member: discord.Member, role: discord.Role
+):
     await member.add_roles(role)
     await channel.send(f"Successfully gave the role {role.name} to {member.name}")
 
@@ -25,6 +25,7 @@ async def remove_role(
     await member.remove_roles(role)
     await channel.send(f"Successfully removed the role {role.name} from {member.name}")
 
+
 @client.event
 async def on_message(message):
     print(message.content, message.author)
@@ -33,21 +34,26 @@ async def on_message(message):
 
     if message.content == "!pay":
         if message.author in get_paid_users():
-            await message.channel.send("You have already paid for access to the channel")
+            await message.channel.send(
+                "You have already paid for access to the channel"
+            )
             return
-        
+
         url = handle_checkout_session()
-        await message.channel.send(f"Click the link below to pay for access to the channel:\n{url}")
-    
+        await message.channel.send(
+            f"Click the link below to pay for access to the channel:\n{url}"
+        )
+
     if message.content == "!add_role":
         for role in message.guild.roles:
             if role.name == PAID_ROLE:
                 await add_role(message.channel, message.author, role)
-    
+
     if message.content == "!remove_role":
         for role in message.guild.roles:
             if role.name == PAID_ROLE:
                 await remove_role(message.channel, message.author, role)
+
 
 @client.event
 async def on_ready():
