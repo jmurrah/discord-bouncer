e18fb9dc27a0b8e04f97260500ca46e111f55028
jmurrah <jacobhmurrah@gmail.com>
2024-05-15 13:49:31 -0500

    completed unit tests for database.py

--- tests/test_database.py
+++ tests/test_database.py
@@ -12,10 +12,13 @@ def test_convert_time_to_date():
         assert database.convert_time_to_date("2024-03-09T02:41:10.000Z") == "2024-03-08"
 
 
-def test_store_member(mock_convert_time_to_date, mock_firestore_document, caplog):
+def test_store_member(mock_convert_time_to_date, mock_firestore_client, caplog):
     caplog.set_level(logging.INFO)
     mock_convert_time_to_date.return_value = "2024-03-08"
-    mock_firestore_document.set.return_value = None
+    mock_document = (
+        mock_firestore_client.return_value.collection.return_value.document.return_value
+    )
+    mock_document.set.return_value = None
 
     member_data = {
         "event": "checkout.session.completed",
@@ -28,7 +31,7 @@ def test_store_member(mock_convert_time_to_date, mock_firestore_document, caplog
     database.store_member(member_data)
 
     assert mock_convert_time_to_date.call_count == 1
-    assert mock_firestore_document.set.call_count == 1
+    assert mock_document.set.call_count == 1
     assert f"Stored member: {member_data}" in caplog.text
 
 
@@ -41,9 +44,12 @@ def test_store_member(mock_convert_time_to_date, mock_firestore_document, caplog
     ],
 )
 def test_access_date_active(
-    member_exists, access_end_date, expected, mock_date, mock_firestore_document
+    member_exists, access_end_date, expected, mock_date, mock_firestore_client
 ):
-    mock_firestore_document.get.return_value = MagicMock(
+    mock_document = (
+        mock_firestore_client.return_value.collection.return_value.document.return_value
+    )
+    mock_document.get.return_value = MagicMock(
         exists=member_exists,
         to_dict=MagicMock(return_value={"access_end_date": access_end_date}),
     )
@@ -54,6 +60,54 @@ def test_access_date_active(
     )
 
     assert database.access_date_active("1234567890") is expected
+    assert mock_document.get.call_count == 1
+    assert mock_date.today.call_count == member_exists
+
+
+def test_delete_expired_members(mock_firestore_client, mock_date, caplog):
+    caplog.set_level(logging.INFO)
+    mock_date.today.return_value = datetime.strptime("2024-03-08", "%Y-%m-%d").date()
+    mock_firestore_client.return_value.collection.return_value.where.return_value.stream.return_value = [
+        MagicMock(to_dict=MagicMock(return_value={"id": "1234567890"})),
+        MagicMock(to_dict=MagicMock(return_value={"id": "0987654321"})),
+    ]
+    mock_firestore_client.return_value.collection.return_value.document.return_value.update.return_value = (
+        MagicMock()
+    )
+
+    database.delete_expired_members()
+
+    assert (
+        mock_firestore_client.return_value.collection.return_value.where.return_value.stream.call_count
+        == 1
+    )
+    assert (
+        mock_firestore_client.return_value.collection.return_value.document.return_value.update.call_count
+        == 1
+    )
+    assert "Deleting 2 expired members." in caplog.text
+    assert "Deleting member: {'id': '1234567890'}" in caplog.text
+    assert "Deleting member: {'id': '0987654321'}" in caplog.text
+
+
+@pytest.mark.parametrize(
+    "deleted_discord_ids, expected",
+    [
+        ({}, []),
+        (
+            {"deleted_discord_ids": ["1234567890", "0987654321"]},
+            ["1234567890", "0987654321"],
+        ),
+    ],
+)
+def test_get_recently_expired_members(
+    deleted_discord_ids, expected, mock_firestore_client
+):
+    mock_firestore_client.return_value.collection.return_value.document.return_value.get.return_value = MagicMock(
+        to_dict=MagicMock(return_value=deleted_discord_ids)
+    )
+
+    assert database.get_recently_expired_members() == expected
 
 
 @pytest.fixture
@@ -63,13 +117,9 @@ def mock_convert_time_to_date():
 
 
 @pytest.fixture
-def mock_firestore_document():
+def mock_firestore_client():
     with patch("google.cloud.firestore.Client") as mock_client:
-        mock_document = MagicMock()
-        mock_client.return_value.collection.return_value.document.return_value = (
-            mock_document
-        )
-        yield mock_document
+        yield mock_client
 
 
 @pytest.fixture
